4550b22d78b4b40b6168bae7eb6d1ce537d6accd
Milton Carlos Katoo <mckatoo@gmail.com>
2020-06-17 14:11:41 -0300

    FIX: REMOVED JWT-DECODE

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -1,8 +1,6 @@
 import { Request, Response, NextFunction } from 'express';
 import jwt from 'jsonwebtoken';
-import jwtDecode from 'jwt-decode'
 import authConfig from '../config/auth.json';
-import {decode} from 'punycode';
 
 const auth = (req: Request, res: Response, next: NextFunction) => {
   const authHeader = req.headers.authorization;
