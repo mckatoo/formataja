734619f303971b4ea9f8fc606e5a638d8712d8b4
Milton Carlos Katoo <mckatoo@gmail.com>
2020-06-25 10:50:48 -0300

    refactor: add cors
    Added cors in app.ts

--- src/app.ts
+++ src/app.ts
@@ -2,15 +2,17 @@
  * @file             : app.ts
  * @author           : Milton Carlos Katoo <mckatoo@gmail.com>
  * Date              : 24.06.2020
- * Last Modified Date: 24.06.2020
+ * Last Modified Date: 25.06.2020
  * Last Modified By  : Milton Carlos Katoo <mckatoo@gmail.com>
  */
+import cors from 'cors'
 import express from 'express'
 
 import routes from './routes'
 
 const app = express()
 
+app.use(cors())
 app.use(express.json())
 app.use(routes)
 
